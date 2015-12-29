77d921e9d0e9a62030fadf9f2170e574e5f29c76
Raymond Zhou <somecrazyidiot@gmail.com>
2015-12-28 23:56:22 -0500

    Remove example code

--- main.js
+++ main.js
@@ -12,14 +12,11 @@ const {
   StatusBarIOS,
   StyleSheet,
   Text,
-  TextInput,
   View,
   YellowBox,
 } = React;
 
 const fontFamilies = require('./font-families');
-const ExBoxes = require('./ExBoxes');
-const ExPhotoGallery = require('./ExPhotoGallery');
 const ExScreen = require('./ExScreen');
 const SimpleMarkdownTextInput = require('./SimpleMarkdownTextInput');
 
@@ -35,12 +32,6 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: '300',
     marginTop: 16,
-    marginHorizontal: HORIZ_SPACE,
-  },
-  paragraph: {
-    color: '#000',
-    fontSize: 16,
-    marginTop: 8,
     marginHorizontal: HORIZ_SPACE,
   },
   text: {
@@ -71,40 +62,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Avenir Next',
     fontSize: 16,
   },
-  note: {
-    color: '#333',
-    fontSize: 14,
-    textAlign: 'center',
-    marginHorizontal: HORIZ_SPACE,
-  },
-  code: {
-    fontFamily: 'Menlo',
-    fontSize: 15,
-  },
-  gallery: {
-    flex: 0,
-    alignSelf: 'center',
-    marginTop: 16,
-    marginBottom: 12,
-  },
-  boxes: {
-    alignSelf: 'center',
-    marginTop: 16,
-    marginBottom: 12,
-  },
-  attribution: {
-    color: '#999',
-    fontWeight: '300',
-    textAlign: 'center',
-    marginTop: 24,
-    marginBottom: 18,
-    marginHorizontal: HORIZ_SPACE,
-  },
-  exponent: {
-    color: '#777',
-    fontWeight: '200',
-    letterSpacing: 3,
-  },
 });
 
 const fontStyles = StyleSheet.create(fontFamilies.reduce((object, fontFamily) => {
@@ -128,16 +85,7 @@ class FirstExperience extends React.Component {
     }
   }
 
-  _handleColorSelected(color) {
-    this.setState({ headerColor: color });
-  }
-
   render() {
-    const boxColors = [
-      '#5ac8fa', '#ffcc00', '#ff9500', '#ff2d55', '#563b7e', '#007aff',
-      '#4cd964', '#ff3b30', '#8e8e93',
-    ];
-
     return (
       <ExScreen
         title="tense"
@@ -171,42 +119,6 @@ class FirstExperience extends React.Component {
             </Text>
           </View>
         )}
-
-        {/* Try editing this text and reloading your project in Exponent */}
-        <Text style={styles.paragraph}>
-          This is a simple example of what you can make with Exponent. Feel
-          free to try modifying it and seeing what happens!
-        </Text>
-
-        {/* Photo gallery demo */}
-        <Text style={styles.sectionTitle}>Photo Gallery</Text>
-        <ExPhotoGallery style={styles.gallery} />
-
-        {/* Bouncy boxes demo */}
-        <Text style={styles.sectionTitle}>Interactive Components</Text>
-        <ExBoxes
-          colors={boxColors}
-          onPressBoxBegin={() => this.setState({ isBoxPressed: true })}
-          onPressBoxEnd={() => this.setState({ isBoxPressed: false })}
-          onSelectColor={this._handleColorSelected.bind(this)}
-          style={styles.boxes}
-        />
-        <Text style={styles.note}>
-          Tap the boxes to change the color of the status bar. Press down
-          and drag them to see them bounce back with spring physics.
-        </Text>
-
-        {/* Publishing instructions */}
-        <Text style={styles.sectionTitle}>Publishing</Text>
-        <Text style={styles.paragraph}>
-          When you are ready to share what your work, run <Text style={styles.code}>exp publish</Text>.
-          Give the link to someone who has the Exponent app and they'll be
-          able to see what you've built.
-        </Text>
-
-        <Text style={styles.attribution}>
-          Made for <Text style={styles.exponent}>EXPONENT</Text>
-        </Text>
 
         {YellowBox}
       </ExScreen>
